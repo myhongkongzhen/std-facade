c5ada7b9de97d1008cff8eff86ee37b778ad822d
Z_Z.W <myhongkongzhen@gmail.com>
2015-12-31 00:03:15 +0800

    添加dobbu測試代碼

---
+++ src/main/java/zzwu/dubbo/std/DemoService.java
@@ -0,0 +1,24 @@
+/**********************************************************************************************************************
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                                       *
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.                        *
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.                                                   *
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.                     *
+ * Vestibulum commodo. Ut rhoncus gravida arcu.                                                                       *
+ **********************************************************************************************************************/
+
+package zzwu.dubbo.std;
+
+/**************************************************************************
+ * <pre>
+ *     FileName: zzwu.dubbo.std.DemoService
+ *         Desc:
+ *       author: Z_Z.W - myhongkongzhen@gmail.com
+ *      version: 2015-12-30 21:56
+ *   LastChange: 2015-12-30 21:56
+ *      History:
+ * </pre>
+ **************************************************************************/
+public interface DemoService
+{
+    String sayHello( String name );
+}
